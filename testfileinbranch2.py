85ecfce854047d2b2c6a63fa89dd87bc1ab9605c
HongrongHuang <huang.hongrong1987@gmail.com>
2017-05-07 00:37:20 +0800

    Update testfileinbranch2.py

--- testfileinbranch2.py
+++ testfileinbranch2.py
@@ -1,6 +1,4 @@
 print "test in the second branch"
-<<<<<<< HEAD
 print "commit 20170506"
-=======
 print "new commit in the second branch"
->>>>>>> refs/remotes/origin/master
+print "2 new commit in the second branch"
